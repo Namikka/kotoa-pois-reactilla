0fe400bb528f1c1fa573da57ddcc48f8ef810255
Kare Salo <kare.salo@gmail.com>
2020-09-08 13:09:23 +0300

    Added a variable for determining if it's probably rain or snow falling outside. Improved comments and rain badge texts.

--- src/forecastHandler.tsx
+++ src/forecastHandler.tsx
@@ -7,10 +7,9 @@ type InfoBadge = {
 };
 /**
 // Return value commented now because I don't want VisualStudioCode to be annoying
-function clothingBadgeGenerator (weatherDataObject: ForecastModel) { //: Array<InfoBadge> { 
-    
-}
- */
+function clothingBadgeGenerator (weatherDataObject: ForecastModel) { //: Array<InfoBadge> {}
+*/
+
 function forecastObjectHandler(weatherDataList: Array<ForecastModel>): Array<ForecastModel> | null {
     // Just in case we got this 
     if (weatherDataList.length < 1) return null;
@@ -20,7 +19,9 @@ function forecastObjectHandler(weatherDataList: Array<ForecastModel>): Array<For
         const weatherSymbol = forecast.WeatherSymbol3;
         const rainAmount = forecast.Rain; // No need to parsefloat because someone didn't set the proper type;
         const badgeArray: Array<InfoBadge> = [];
+        const isItWinter: boolean = temperature < -4;
 
+        // Custom badges for slush and stuff.
         if (weatherSymbol === "82.0") {
             badgeArray.push({ name: "Räntää", level: "warning", type: "condition" });
         } else if (weatherSymbol === "67.0") {
@@ -28,6 +29,8 @@ function forecastObjectHandler(weatherDataList: Array<ForecastModel>): Array<For
         } else if (weatherSymbol === "59.0") {
             badgeArray.push({ name: "Paljon lunta", level: "warning", type: "condition" });
         }
+        
+        // WIND
         if (windSpeed > 15) {
             badgeArray.push({ name: "KOVAA TUULTA", level: "danger", type: "wind" });
         }
@@ -37,6 +40,7 @@ function forecastObjectHandler(weatherDataList: Array<ForecastModel>): Array<For
         if (8 >= windSpeed && windSpeed > 4) {
             badgeArray.push({ name: "Tuulista", level: "info", type: "wind" });
         }
+        // TEMPERATURE
         if (temperature <= -25) {
             badgeArray.push({ name: "KOVA PAKKANEN", level: "danger", type: "temperature"  });
         }
@@ -56,25 +60,34 @@ function forecastObjectHandler(weatherDataList: Array<ForecastModel>): Array<For
             badgeArray.push({ name: "Lämmintä", level: "info", type: "temperature" });
         }
         if (23 < temperature && temperature <= 27) {
-            badgeArray.push({ name: "Hellettä", level: "warning", type: "temperature"  });
+            badgeArray.push({ name: "Helle", level: "warning", type: "temperature"  });
         }
         if (27 < temperature) {
             badgeArray.push({ name: "KOVA HELLE", level: "danger", type: "temperature"  });
         }
+        // RAIN / SNOW
         if (rainAmount < 1 && rainAmount > 0) {
+            (isItWinter) ? 
+            badgeArray.push({ name: "Vähän lunta", level: "info", type: "rain" }) : 
             badgeArray.push({ name: "Tihuttaa", level: "info", type: "rain" });
+            
         }
-        if (rainAmount < 3 && rainAmount > 1) {
-            badgeArray.push({ name: "Sataa", level: "info", type: "rain"  });
+        if (rainAmount < 3 && rainAmount >= 1) {
+            (isItWinter) ? 
+            badgeArray.push({ name: "Sataa lunta", level: "info", type: "rain" }) : 
+            badgeArray.push({ name: "Sataa", level: "info", type: "rain" });
         }
-        if (rainAmount < 6 && rainAmount > 3) {
-            badgeArray.push({ name: "Sataa paljon", level: "warning", type: "rain" });
+        if (rainAmount < 6 && rainAmount >= 3) {
+            (isItWinter) ? 
+            badgeArray.push({ name: "Sataa paljon lunta", level: "info", type: "rain" }) : 
+            badgeArray.push({ name: "Sataa paljon", level: "info", type: "rain" });
         } 
         if (rainAmount > 6) {
-            badgeArray.push({ name: "VITUSTI VETTÄ", level: "danger", type: "rain"  });
+            (isItWinter) ? 
+            badgeArray.push({ name: "LUNTA PALJON", level: "danger", type: "rain" }) : 
+            badgeArray.push({ name: "VITUSTI VETTÄ", level: "danger", type: "rain" });
         }
         forecast.BadgeList = badgeArray;
-        // Then custom badges for slush and stuff.
         return forecast;
     });
     return badgedWeatherDataList;
